33ed84449a332b136e30bed21f85d1bd0bd84a80
Thando <thando.vince@gmail.com>
2023-06-18 15:11:26 -0700

    added file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char alph = "abcdefghijklmnopqqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+
+for (i = 0; i < 52; i++)
+{
+	putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
