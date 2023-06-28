b38f05fe993dec48af33e5f97066c5b7450edbb8
Thando <thando.vince@gmail.com>
2023-06-28 10:57:25 -0700

    added file 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * All the prototypes for 0x06. C - more pointers, arrays,and strings
+ */
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
+
+#endif
