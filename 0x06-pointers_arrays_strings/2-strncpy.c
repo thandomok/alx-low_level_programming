71c7285b33718d73e45a442d6f4ff09757de3851
Thando <thando.vince@gmail.com>
2023-06-29 13:23:20 -0700

    fixed the errors

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int count_of_bytes;
 
-	for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] !
-	'\0'; count_of_bytes++)
-
-	dest[count_of_bytes] = src[count_of_bytes];
+	for (count_of_bytes = 0; count_of_bytes < n
+			&& src[count_of_bytes] != '\0'; count_of_bytes++)
+	{
+		dest[count_of_bytes] = src[count_of_bytes];
+	}
 
 	for (; count_of_bytes < n; count_of_bytes++)
-	dest[count_of_bytes] = '\0';
+	{
+		dest[count_of_bytes] = '\0';
+	}
 	return (dest);
 }
