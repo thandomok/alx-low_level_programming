565036e1f7f1ad14a96005c1b347e9c87f70a578
Thando <thando.vince@gmail.com>
2023-06-29 14:28:36 -0700

    fixed errors

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,32 +3,34 @@
  * cap_string - a function that capitalizes all words of a string.
  * @s: pointer to string.
  *
- * return: pointer to s.
+ * Return: pointer to s.
  */
 char *cap_string(char *s)
 {
-	int string-count;
-/* scan through string */
+	int string_count;
+
 	string_count = 0;
 	while (s[string_count] != '\0')
-	{/* if next character after count is a char , capitalise it */
-		if (s[0] >= 97 && s[0] <= 122)
-
-			s[0] = s[0] - 32;
-	}
-	if (s[string_count] == ' ' || s[string_count] == '\t' || s[string_count] == '\
-	    || s[string_count] == ' , ' || s[string_count] == ' , ' || s[string_count] == ' , '
-	    || s[string_count] == ' , ' || s[string_count] == ' ! ' || s[string_count] == ' ? '
-	    || s[string_count] == ' " ' || s[string_count] == ' ( ' || s[string_count] == ' ) '
-	    || s[string_count] == ' { ' || s[string_count] == ' } ')
 	{
-		if (s[string_count] == ' ' || s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
+		if (s[0] >= 97 && s[0] <= 122)
 		{
-			s[string_count + 1] = s[string_count + 1] - 32
+			s[0] = s[0] - 32;
+		}
+	}
+	if (s[string_count] == ' ' || s[string_count] == '\t'
+		|| s[string_count] == '\n' || s[string_count] == ','
+			|| s[string_count] == ',' || s[string_count] == ','
+			|| s[string_count] == ',' || s[string_count] == '!'
+			|| s[string_count] == '?' || s[string_count] == '\"'
+			|| s[string_count] == '(' || s[string_count] == ')'
+			|| s[string_count] == '{' || s[string_count] == '}')
+	{
+		if ((s[string_count] == ' ' || s[string_count + 1] >= 97) &&
+				s[string_count + 1] <= 122)
+		{
+			s[string_count + 1] = s[string_count + 1] - 32;
 		}
 	}
 	string_count++;
 	return (s);
 }
-	
-	
