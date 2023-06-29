ecb2f1e611e30d5116b0c9dadf403aef3920bde8
Thando <thando.vince@gmail.com>
2023-06-29 13:07:20 -0700

    fixed betty errors

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,10 +11,9 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int length_of_string, z;
-/* z is a counter for n bytes of src to be concatenated */
-/* length_of_string - length of destination string */
 
 	length_of_string = 0;
+
 	while (dest[length_of_string] != '\0')
 	{
 		length_of_string++;
@@ -26,6 +25,3 @@ char *_strncat(char *dest, char *src, int n)
 	dest[length_of_string] = '\0';
 	return (dest);
 }
-
-             
-        
